c2892418473bbd8ad578118bdae818d35a65865c
jmtiv <jmt5ce@virginia.edu>
2017-02-06 10:51:48 -0500

    Update kardsusweekly.py

--- kardsusweekly.py
+++ kardsusweekly.py
@@ -1,24 +1,52 @@
 #!/usr/bin/env python
+import csv
 import datetime
 from dateutil import parser
 import urllib2
 from bs4 import BeautifulSoup
 
-article_url = 'http://www.usmagazine.com/entertainment/news/kim-kardashian-worries-over-247-security-after-paris-robbery-w464533'
 
-soup = BeautifulSoup(urllib2.urlopen(article_url).read(),'html.parser')
 
-title = soup.h1.contents[0]
-print title
+ARTICLE_CSV = 'usweekly-articles.csv'
+TEXT_CSV = 'usweekly-text.csv'
 
-article_date = soup.time.contents[0]
-try:
-    article_date = parser.parse(article_date, fuzzy=True)
-except:
-    print('ERROR ON', article_date)
-    raise
-print article_date
+def download_url (article_url):
+    """Download article_url and return the title, date, and text."""
 
-article_text = soup.main
-[aside.extract() for aside in article_text.find_all("aside")]
-print article_text.get_text()
+    soup = BeautifulSoup(urllib2.urlopen(article_url).read(),'html.parser')
+
+    title = soup.h1.contents[0]
+
+
+    article_date = soup.time.contents[0]
+    try:
+        article_date = parser.parse(article_date, fuzzy=True)
+    except:
+        print('ERROR ON', article_date)
+        raise
+
+
+    article_text = soup.main
+    for aside in article_text.find_all("aside"):
+        aside.extract()
+
+
+    return (title, article_date, article_text.get_text())
+
+with open (ARTICLE_CSV, 'rU') as file_in:
+    with open (TEXT_CSV, 'w') as file_out:
+        reader = csv.reader (file_in)
+        writer =csv.writer (file_out)
+
+        writer.writerow (['url', 'title', 'date', 'text'])
+        reader.next()
+        for row in reader:
+            url = row [1]
+            print (url)
+            title, date, text = download_url (url)
+            writer.writerow ([
+                url,
+                title.encode ('utf8'),
+                date,
+                text.encode ('utf8'),
+                ])
